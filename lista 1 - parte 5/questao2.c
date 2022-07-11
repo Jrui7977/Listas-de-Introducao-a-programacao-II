29afb2d6b66bb545df9ce751f66bd1b8569b2847
José Rui <60098531+Jrui7977@users.noreply.github.com>
2022-07-10 21:51:47 -0300

    Update questao2.c

--- lista 1 - parte 5/questao2.c
+++ lista 1 - parte 5/questao2.c
@@ -6,7 +6,7 @@ int findprimos(int n){
 		return 0;
 	}
 	for(x = 2; x < n;x++){
-		if(x%2 != 0 && x%3!=0 && x%7!=0 && x%5!=0|| x == 2 || x == 3){
+		if(x%2 != 0 && x%3!=0 && x%7!=0 && x%5!=0|| x == 2 || x == 3 || x==5 || x == 7){
 			printf("\n[%d]",x);
 		}
 	}
